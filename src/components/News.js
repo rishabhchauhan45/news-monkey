dc0023dd72daf9208d8619aa123e1efa259625c1
student rishabh chauhan <rishabhchauhan1434@gmail.com>
2025-11-14 19:54:10 +0530

    Day 2 work done

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
-export class news extends Component {
+export class News extends Component { // Capital 'N' का ध्यान रखें
+
+  
+ 
+ constructor() {
+    super();
+    console.log("hello i am constructor from news component");
+    this.state = {
+      articles: [], 
+      loading: false
+    }
+  }
+
+async componentDidMount() {
+   
+let url = "https://newsdata.io/api/1/latest?apikey=pub_1136e01a11c34ae482238053f60f2961&q=ind&language=en";
+    
+    try {
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData); 
+        this.setState({ 
+            articles: parsedData.results ? parsedData.results : [],
+            loading: false
+        });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
   render() {
+    console.log("render");
     return (
-      <div>
-        this is a news components
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+      <div className="row">
+    {this.state.articles && this.state.articles.map((element) => {
+        return <div className='col-md-4' key={element.link}>
+            <NewsItem 
+                title={element.title ? element.title.slice(0, 45) : ""}
+                description={element.description ? element.description.slice(0, 88) : ""}
+                imageUrl={element.image_url}
+                newsUrl={element.link} 
+            />
         </div>
+    })}
+</div>
+        
+      </div>
     )
   }
 }
 
-export default news
+export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+ 
   render() {
+    let { title, description ,imageUrl, newsUrl} = this.props;
     return (
-      <div>
-        this is a news item
-        <NewsItem/>
+      <div className="my-3">
+       
+        <div className="card" style={{ width: "18rem" }}>
+        
+          <img src={!imageUrl?"https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2246192352.jpg?c=16x9&q=w_800,c_fill": imageUrl}className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}...</p>
+
+          <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
