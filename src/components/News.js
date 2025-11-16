9a2bf6197e3ca1317be4f952cfdbd1565887bd38
student rishabh chauhan <rishabhchauhan1434@gmail.com>
2025-11-16 23:17:09 +0530

    day3

--- src/components/News.js
+++ src/components/News.js
@@ -1,64 +1,88 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
-export class News extends Component { 
-  
- 
- constructor() {
-    super();
-    console.log("hello i am constructor from news component");
-    this.state = {
-      articles: [], 
-      loading: false
-    }
-  }
+export class News extends Component {
 
-async componentDidMount() {
-   
-let url = "https://newsdata.io/api/1/latest?apikey=pub_1136e01a11c34ae482238053f60f2961&q=ind&language=en";
+    constructor() {
+        super();
+        console.log("hello i am constructor from news component");
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1, 
+            nextPage: null
+        }
+    }
+
+    async componentDidMount() {
+    
+    let url = `https://newsdata.io/api/1/latest?apikey=pub_1136e01a11c34ae482238053f60f2961&q=ind&language=enpageSize=30`;
     
     try {
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData); 
-        this.setState({ 
+
+      
+        this.setState({
             articles: parsedData.results ? parsedData.results : [],
+            nextPage: parsedData.nextPage, 
             loading: false
         });
     } catch (error) {
         console.error("Error fetching data:", error);
     }
 }
-handlePrevClick = ()=>{
-console.log("previous")
-}
-handleNextClick = ()=>{
-console.log("next")
-}
-  render() {
-    console.log("render");
-    return (
-      <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
-      <div className="row">
-    {this.state.articles && this.state.articles.map((element) => {
-        return <div className='col-md-4' key={element.link}>
-            <NewsItem 
-                title={element.title ? element.title.slice(0, 45) : ""}
-                description={element.description ? element.description.slice(0, 88) : ""}
-                imageUrl={element.image_url}
-                newsUrl={element.link} 
-            />
-        </div>
-    })}
-</div>
-        <div className="container d-flex justify-content-between" >
-          <button type="button" class="btn btn-dark" onClick={this.handleNextClick}> &larr; previous</button>
-          <button type="button" class="btn btn-dark" onClick={this.handleNextClick}> Next &rarr;</button>
-        </div>
-      </div>
-    )
-  }
-}
 
+    handlePrevClick = async () => {
+        console.log("previous");
+        let url = `https://newsdata.io/api/1/latest?apikey=pub_1136e01a11c34ae482238053f60f2961&q=ind&language=en&page=${this.state.page - 1}&pageSize-30`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.results
+        });
+    }
+
+    handleNextClick = async () => {
+        console.log("Next button clicked");
+        let url = `https://newsdata.io/api/1/latest?apikey=pub_1136e01a11c34ae482238053f60f2961&q=ind&language=en&page=${this.state.page + 1}&pageSize-30`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.results
+        });
+    }
+
+   
+    render() {
+        console.log("render");
+        return (
+            <div className="container my-3">
+            <h1 className="text-center"> NewsMonkey - Top Headlines</h1>
+
+                <div className="row">
+                    {this.state.articles && this.state.articles.map((element) => {
+                        return <div className='col-md-4' key={element.link}>
+                            <NewsItem
+                                title={element.title ? element.title.slice(0, 45) : ""}
+                                description={element.description ? element.description.slice(0, 88) : ""}
+                                imageUrl={element.image_url}
+                                newsUrl={element.link}
+                            />
+                        </div>
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                   
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
+} 
 export default News
